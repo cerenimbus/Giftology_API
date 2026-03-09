29bedcf2e9dc31f368d33120eaf750aafd09afd8
Christian <capoquiancapz12@gmail.com>
2026-03-09 15:07:23 +0800

    Update GetContact.php

--- GetContact.php
+++ GetContact.php
@@ -264,3 +264,35 @@ $output = '<ResultInfo>
 
 send_output($output);
 exit;
+<?php
+include("db_connection.php");
+
+// JMF 03/09/26 Applied mysqli_real_escape_string to sanitize input values from request
+
+$contactID = "";
+$userID = "";
+
+if(isset($_GET['contactID'])){
+    $contactID = mysqli_real_escape_string($conn, $_GET['contactID']);
+}
+
+if(isset($_POST['contactID'])){
+    $contactID = mysqli_real_escape_string($conn, $_POST['contactID']);
+}
+
+if(isset($_REQUEST['userID'])){
+    $userID = mysqli_real_escape_string($conn, $_REQUEST['userID']);
+}
+
+$query = "SELECT * FROM contacts WHERE contactID = '$contactID' OR userID = '$userID'";
+
+$result = mysqli_query($conn, $query);
+
+$contacts = array();
+
+while($row = mysqli_fetch_assoc($result)){
+    $contacts[] = $row;
+}
+
+echo json_encode($contacts);
+?>
